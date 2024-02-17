3611710fe6a906f5632ac3c96443bfd4429893e1
Technita <raagaleugwuru@gmail.com>
2024-02-17 15:31:20 -0800

    size ia an attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,8 +28,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-
-vi 5-square
-#!/usr/bin/python3
-"""Define a class Square."""
-
